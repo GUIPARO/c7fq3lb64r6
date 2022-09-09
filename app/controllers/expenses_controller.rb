f8031826847e3e22c7c49378fa16eb7a93f5efcc
GUIPARO <guisela2728@gmail.com>
2022-09-09 00:20:50 -0500

    change controller index method

--- app/controllers/expenses_controller.rb
+++ app/controllers/expenses_controller.rb
@@ -1,5 +1,8 @@
 class ExpensesController < ApplicationController
   def index
-   @expenses = Expense.order("date DESC")
+    @expenses = Expense.where("concept LIKE ? AND category_id LIKE ?",
+      "%#{params[:concept]}%",
+      "%#{params[:category_id]}%")
+
   end
 end
